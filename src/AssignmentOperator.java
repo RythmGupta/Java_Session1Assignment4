171d6347f7f25fa7b44073fdaa86639d886768d3
RythmGupta <sunrit.rythm@gmail.com>
2016-08-14 07:22:15 -0700

    Java_Session1Assignment4

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,11 @@
+
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int x=5,y=10;
+    System.out.println(x+y*2);
+    System.out.println(x-y+2);
+    System.out.println((x+y)*2);
+    System.out.println(y/x);
+	}
+}
